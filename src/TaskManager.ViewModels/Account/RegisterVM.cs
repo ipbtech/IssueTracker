f428bc9d59fb9971ca29a6db236689590d49c062
Петров Илья Вадимович <iv.petrov@samolet.ru>
2024-11-16 01:19:17 +0300

    layout and register page

---
+++ src/TaskManager.ViewModels/Account/RegisterVM.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.ViewModels.Account
+{
+    public class RegisterVM
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9\\!@#$%^&*()_+\\-=\\[\\]{};':]", ErrorMessage = "Только буквы латинского алфавита, цифры и спецсимволы")]
+        public string Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароли должны совпадать")]
+        public string ConfirmPassword { get; set; }
+    }
+}
--- src/TaskManager.WebApp/Controllers/AccountController.cs
+++ src/TaskManager.WebApp/Controllers/AccountController.cs
@@ -4,6 +4,18 @@ namespace TaskManager.WebApp.Controllers
 {
     public class AccountController : Controller
     {
+        
+        
+        
+        
+        
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        
+        
         public IActionResult Login()
         {
             return View();
